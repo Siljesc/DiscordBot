92d2ff46ab7236cde9d260bccf52ae6690e0ed4d
sunrisem <sunrisem.js@gmail.com>
2018-12-26 00:19:24 -0400

    Implement config method name change

--- src/events/Message.ts
+++ src/events/Message.ts
@@ -3,7 +3,7 @@ import { Message } from "discord.js";
 
 export function onMessage(instance: IInstance, message: Message) {
 	if (message.author.bot) return;
-	if (!message.content.startsWith(instance.config.byID(message.guild.id).values.prefix)) return;
+	if (!message.content.startsWith(instance.config.fromID(message.guild.id).values.prefix)) return;
 
 	instance.commandHandler.parse(message, instance.config);
 }
