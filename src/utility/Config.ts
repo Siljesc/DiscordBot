4f143dccabe02b014dd96305e4660a8e604a051a
sunrisem <sunrisem.js@gmail.com>
2018-12-26 00:22:11 -0400

    Refactor GuildConfig properties into interfaces

--- src/utility/Config.ts
+++ src/utility/Config.ts
@@ -8,22 +8,36 @@ export interface IConfigMessages {
 }
 
 export interface IConfig {
-	messages: {
-		InvalidCommandName: "Invalid Command";
-		MissingUserPermissions: "You don't have permissions for this";
-		MissingClientPermissions: "Bot doesn't have permissions for this";
-		CommandHandlingError: "This command is broken";
-	};
-	prefix: "!";
+	messages: IConfigMessages
+	prefix: string
 }
 
-export class GuildConfig {
-	InvalidCommandName: string;
-	MissingUserPermissions: string;
-	MissingClientPermissions: string;
-	CommandHandlingError: string;
+export const guildConfigValues : IConfig = {
+	messages: {
+		InvalidCommandName: "Invalid Command",
+		MissingUserPermissions: "You don't have permissions for this",
+		MissingClientPermissions: "Bot doesn't have permissions for this",
+		CommandHandlingError: "This command is broken",
+	},
+	prefix: "!"
+};
 
-	constructor(guildID: Snowflake) {}
+export class GuildConfig {
+	private _values: IConfig;
+	public id: Snowflake;
+
+	constructor(id: Snowflake) {
+		this.id = id;
+		this._values = guildConfigValues;
+	}
+
+	get values(){
+		return this._values;
+	}
+
+	set values(newValue){
+		this._values = newValue;
+	}
 }
 
 export default class Config {
@@ -31,7 +45,7 @@ export default class Config {
 		[guildID: string]: GuildConfig;
 	} = {};
 
-	byID(guildID: Snowflake) {
+	fromID(guildID: Snowflake){
 		if (!this.guilds[guildID]) this.guilds[guildID] = new GuildConfig(guildID);
 		return this.guilds[guildID];
 	}
