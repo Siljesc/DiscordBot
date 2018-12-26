56cf038866604d291dff8b871b8a241325ba2481
sunrisem <sunrisem.js@gmail.com>
2018-12-26 00:19:45 -0400

    Fix typo

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -3,7 +3,7 @@ import * as mongoose from "mongoose";
 export function connect() {
 	const [url, dbName] = ["DB_URL", "DB_NAME"].map(name => {
 		const env = process.env[name];
-		if (!env) throw new Error(`Missing ${name} enviroment variable`);
+		if (!env) throw new Error(`Missing ${name} environment variable`);
 		return env;
 	});
 
